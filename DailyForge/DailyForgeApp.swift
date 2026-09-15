b4020b75ae38493df5e26baee7a021bc7372617a
mocodesu <mohkafdesu@gmail.com>
2026-09-15 16:32:40 +0300

    Initial Commit

---
+++ DailyForge/DailyForgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyForgeApp.swift
+//  DailyForge
+//
+//  Created by Mohamed Abdikafi Abdi on 15/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct DailyForgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
